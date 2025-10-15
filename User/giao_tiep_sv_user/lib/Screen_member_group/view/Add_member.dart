c6e0e830d7e84a6dd4e04bea1ada83f93cb16042
hiep hoc git <lehiep09052005@gmail.com>
2025-10-15 09:31:42 +0700

    fix add member 1

--- User/giao_tiep_sv_user/lib/Screen_member_group/view/Add_member.dart
+++ User/giao_tiep_sv_user/lib/Screen_member_group/view/Add_member.dart
@@ -10,6 +10,37 @@ class AddMemberScreen extends StatefulWidget {
 }
 
 class _AddMemberScreenState extends State<AddMemberScreen> {
+  List<CustommemberWidget> Listsearch = [];
+  List<CustommemberWidget> ListMember = [
+    CustommemberWidget(
+      id: 1,
+      url: "assets/images/avatar.png",
+      fullname: "Le Dai Hiep",
+    ),
+    CustommemberWidget(
+      id: 2,
+      url: "assets/images/avatar.png",
+      fullname: "Le Dinh Thuan",
+    ),
+    CustommemberWidget(
+      id: 3,
+      url: "assets/images/avatar.png",
+      fullname: "Cao Quang Khanh",
+    ),
+    CustommemberWidget(
+      id: 4,
+      url: "assets/images/avatar.png",
+      fullname: "Pham Thang",
+    ),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Listsearch = ListMember;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,29 +69,50 @@ class _AddMemberScreenState extends State<AddMemberScreen> {
           ),
         ),
       ),
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Customsearch(
-            onChanged: (value) {
-              print("$value");
-            },
-          ),
-          SizedBox(height: 10),
-          Text("Kết quả:"
-          ,style: TextStyle(
-            color: Colors.black.withOpacity(0.6),
-            fontSize:15 
-          ),),
-          SizedBox(height: 10),
-          CustommemberWidget(
-            id: 1,
-            url: "assets/images/avatar.png",
-            fullname: "Le Dai Hiep",
-          ),
-        ],
-      ),)
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Customsearch(
+              onChanged: (value) {
+                Listsearch = ListMember.where((element) {
+                  return element.fullname.toLowerCase().contains(
+                    value.toLowerCase(),
+                  );
+                }).toList();
+                print("$value");
+              },
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Kết quả:1",
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.6),
+                fontSize: 15,
+              ),
+            ),
+            SizedBox(height: 10),
+            create_listMember(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget create_listMember() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: Listsearch.length,
+      itemBuilder: (context, index) {
+        var value = Listsearch[index];
+        return CustommemberWidget(
+          id: value.id,
+          url: value.url,
+          fullname: value.fullname,
+        );
+      },
     );
   }
 }
--- User/giao_tiep_sv_user/lib/main.dart
+++ User/giao_tiep_sv_user/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:giao_tiep_sv_user/Screen_member_group/view/Add_member.dart';
 import 'Home_screen/home.dart'; // import trang Home bạn tạo
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Home(), // Trang mặc định
+      home: AddMemberScreen(), // Trang mặc định
     );
   }
 }
