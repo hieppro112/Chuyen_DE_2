8307bc32eeeee3a97c3e512eded1ccecc09cc8a3
hiep hoc git <lehiep09052005@gmail.com>
2025-10-27 13:28:07 +0700

    temp

--- User/giao_tiep_sv_user/lib/Profile/profile.dart
+++ User/giao_tiep_sv_user/lib/Profile/profile.dart
@@ -5,7 +5,6 @@ import 'package:giao_tiep_sv_user/Profile/editProflie/edit_profile_screen.dart';
 import 'package:giao_tiep_sv_user/Profile/personalPost/personal_post_screen.dart';
 import 'package:giao_tiep_sv_user/Profile/saveItemsProfile/saved_items_profile_screen.dart';
 import 'package:giao_tiep_sv_user/duyet_Nguoi_Dung/member_post_screen.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
