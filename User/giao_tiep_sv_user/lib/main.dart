b5d37305212d9bc80236d9fff4dc3fc2f1e237cc
hieppro112 <lehiep08052005@gmail.com>
2025-10-30 10:23:51 +0700

    hiep connect firebase

--- User/giao_tiep_sv_user/lib/main.dart
+++ User/giao_tiep_sv_user/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:giao_tiep_sv_user/Home_screen/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
