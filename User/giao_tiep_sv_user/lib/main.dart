3aea4a57245c5646fdb57f3fdd5471fe6315393c
CQKhanh2404 <ckhanh2404@gmail.com>
2025-11-05 13:14:25 +0700

    save status

--- User/giao_tiep_sv_user/lib/main.dart
+++ User/giao_tiep_sv_user/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
 
-      home: DangNhap() // Trang mặc định
+      home: Home(), // Trang mặc định
     );
   }
 }
